7e8dfc8213ef26cf1184fc162e07755edae9c207
unknown <arushadfaisal@gmail.com>
2026-09-21 21:33:07 +0500

    create navbar

--- frontend/src/components/layout/bottomNavBar.tsx
+++ frontend/src/components/layout/bottomNavBar.tsx
@@ -1,18 +1,30 @@
 import {useState} from 'react';
+import {House, InfoIcon, SendIcon} from 'lucide-react';
 
 const BottomNavBar = () => {
     const [activeTab, setActiveTab] = useState('home');
 
-    return (
-        <div className="fixed bottom-0 left-0 right-0 bg-gray-800 text-white flex justify-around p-4">
+    const navButton = (tab: string, icon: React.ReactNode, label: string) => {
+        return (
             <button
-                className={`flex flex-col items-center ${activeTab === 'home' ? 'text-blue-500' : ''}`}
-                onClick={() => setActiveTab('home')}
+                className={`flex flex-col rounded-full p-3 px-5 items-center
+                    ${activeTab === tab ? 'text-blue-300 bg-gray-600' : 'text-slate-200 bg-gray-800'}
+                    transition duration-200 ease-in-out transform 
+                    hover:scale-105 hover:bg-gray-700 active:scale-80 cursor-pointer`}
+                onClick={() => setActiveTab(tab)} type="button"
             >
-                Home
+                {icon}
+                <span>{label}</span>
             </button>
+        )
+    }
+
+    return (
+        <div className="fixed bottom-0 left-0 right-0 flex justify-around bg-gray-800 p-2">
+            {navButton('about', <InfoIcon className="w-8 h-8" />, 'About')}
+            {navButton('home', <House className="w-8 h-8" />, 'Home')}
+            {navButton('contact', <SendIcon className="w-8 h-8" />, 'Contact')}
         </div>
     )
 }
-
 export default BottomNavBar;
